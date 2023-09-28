584974c564f753842a4b6af4041198877eb28ca5
Khagendra Kumar <itskhagendra@hotmail.com>
2023-09-28 16:09:11 +0530

    Added Wireframe Style
    --Added Bootstrap CSS
    --Created Wireframe UI

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import style from "./App.module.css";
 
 function App() {
   return (
     <>
-    <h1>Hello</h1>
+      <div className={style.mainContainer}>
+        <div className={style.headerContainer}>
+          <div className="row">
+            <div className="col-4">
+              <div>Logo</div>
+              <div>Subtitle</div>
+            </div>
+            <div className="col-md-12 col-lg-4">
+              <input type="text"/>
+            </div>
+          </div>
+        </div>
+        <div className={style.tvShows}>
+          TV Show Detail
+        </div>
+        <div className={style.recommendedShow}>Recommended Shows</div>
+      </div>
     </>
   );
 }
